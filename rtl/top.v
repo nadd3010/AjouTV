1a3d67337d7f6286d6e0a6aeb9a179bd4ef32a3e
nadd3010 <suin2.2005@gmail.com>
2026-06-30 18:01:19 +0900

    RTL 모듈 템플릿 14개 생성

---
+++ rtl/top.v
@@ -0,0 +1,12 @@
+module top (
+    input  wire       clk,
+    input  wire       rst_n,
+    input  wire       rx,
+    output wire       tx,
+    output wire [3:0] led,
+    output wire       buzzer
+);
+
+// TODO: 전체 모듈 인스턴스 연결
+
+endmodule
